e670af72c2599c5b9e287e8e13f58409350e0813
Jeffsul <jeffsul16@gmail.com>
2014-09-12 20:04:03 -0400

    Fixing formatting in highscore engine.

--- src/com/jeffsul/minesweeper/highscores/HighScoreEngine.java
+++ src/com/jeffsul/minesweeper/highscores/HighScoreEngine.java
@@ -9,124 +9,111 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class HighScoreEngine 
-{	
+public class HighScoreEngine {	
 	public static enum SortDirection {ASC, DESC};
 	public static enum Type {INTEGER, STRING, CHAR, DOUBLE};
 	
 	private File highScoreFile;
 	private Type[] types;
 	
-	public HighScoreEngine(String file, Type[] types)
-	{
+	public HighScoreEngine(String file, Type[] types) {
 		highScoreFile = new File(file);
 		this.types = types;
 	}
 	
-	public void setFile(String file)
-	{
+	public void setFile(String file) {
 		highScoreFile = new File(file);
 	}
 	
-	public void setTypes(Type[] types)
-	{
+	public void setTypes(Type[] types) {
 		this.types = types;
 	}
 	
-	public Object[][] load()
-	{
+	public Object[][] load() {
 		ArrayList<Object[]> resultSet = new ArrayList<Object[]>();
 		BufferedReader reader;
-		try 
-		{
+		try {
 			reader = new BufferedReader(new FileReader(highScoreFile));
 			String line;
-			while ((line = reader.readLine()) != null)
+			while ((line = reader.readLine()) != null) {
 				resultSet.add(new Object[] { line });
+			}
 			reader.close();
-		} catch (FileNotFoundException exception) { }
-		catch (IOException exception) { }
+		} catch (FileNotFoundException exception) {}
+		catch (IOException exception) {}
 		
 		Object[][] result = new Object[resultSet.size()][];
 		resultSet.toArray(result);
 		return result;
 	}
 	
-	public Object[][] load(String delim)
-	{
+	public Object[][] load(String delim) {
 		return load(delim, Integer.MAX_VALUE);
 	}
 	
-	public Object[][] load(String delim, int max)
-	{
+	public Object[][] load(String delim, int max) {
 		ArrayList<Object[]> resultSet = new ArrayList<Object[]>();
 		BufferedReader reader;
-		try 
-		{
+		try {
 			reader = new BufferedReader(new FileReader(highScoreFile));
 			String line;
 			int i = 0;
-			while ((line = reader.readLine()) != null && i < max)
-			{
+			while ((line = reader.readLine()) != null && i < max) {
 				Object[] vals = parse(line.split(delim));
-				if (vals != null)
+				if (vals != null) {
 					resultSet.add(vals);
+				}
 				i++;
 			}
 			reader.close();
-		} catch (FileNotFoundException exception) { }
-		catch (IOException exception) { }
+		} catch (FileNotFoundException exception) {}
+		catch (IOException exception) {}
 		
 		Object[][] result = new Object[Math.min(max, resultSet.size())][];
 		resultSet.toArray(result);
 		return result;
 	}
 	
-	public Object[][] load(String delim, int max, int sortField)
-	{
+	public Object[][] load(String delim, int max, int sortField) {
 		return load(delim, max, sortField, SortDirection.ASC);
 	}
 	
-	public Object[][] load(String delim, int max, int sortField, SortDirection dir)
-	{
+	public Object[][] load(String delim, int max, int sortField, SortDirection dir) {
 		return load(delim, max, sortField, dir, new HighScoreFilter[] { });
 	}
 	
-	public Object[][] load(String delim, int max, int sortField, SortDirection dir, HighScoreFilter[] filters)
-	{
+	public Object[][] load(String delim, int max, int sortField, SortDirection dir, HighScoreFilter[] filters) {
 		ArrayList<Object[]> resultSet = new ArrayList<Object[]>();
 		BufferedReader reader;
-		try 
-		{
+		try {
 			reader = new BufferedReader(new FileReader(highScoreFile));
 			String line;
-			while ((line = reader.readLine()) != null)
-			{
+			while ((line = reader.readLine()) != null) {
 				Object[] lineVal = parse(line.split(delim));
 				boolean skip = (lineVal == null);
-				for (int i = 0; i < filters.length && !skip; i++)
-					if (!filters[i].match(lineVal))
+				for (int i = 0; i < filters.length && !skip; i++) {
+					if (!filters[i].match(lineVal)) {
 						skip = true;
+					}
+				}
 				
-				if (!skip)
-				{
+				if (!skip) {
 					Object value = lineVal[sortField];
 					boolean added = false;
-					for (int i = 0; i < resultSet.size() && !added; i++)
-					{
-						if (compare(value, resultSet.get(i)[sortField]) && dir == SortDirection.DESC)
-						{
+					for (int i = 0; i < resultSet.size() && !added; i++) {
+						if (compare(value, resultSet.get(i)[sortField]) && dir == SortDirection.DESC) {
 							resultSet.add(i, lineVal);
 							added = true;
 						}
 					}
-					if (!added)
+					if (!added) {
 						resultSet.add(lineVal);
+					}
 				}
 			}
 			reader.close();
-		} catch (FileNotFoundException exception) { }
-		catch (IOException exception) { }
+		} catch (FileNotFoundException exception) {}
+		catch (IOException exception) {}
 		
 		int size = Math.min(max, resultSet.size());
 		Object[][] result = new Object[size][];
@@ -134,33 +121,34 @@ public class HighScoreEngine
 		return result;
 	}
 	
-	public boolean add(Object[] values, String delim)
-	{
+	public boolean add(Object[] values, String delim) {
 		BufferedWriter writer = null;
-		try
-		{
+		try {
 			writer = new BufferedWriter(new FileWriter(highScoreFile, true));
-			if (highScoreFile.length() != 0L)
+			if (highScoreFile.length() != 0L) {
 				writer.write('\n');
-			for (int i = 0; i < values.length - 1; i++)
+			}
+			for (int i = 0; i < values.length - 1; i++) {
 				writer.write(values[i].toString() + delim);
+			}
 			writer.write(values[values.length - 1].toString());
 			writer.close();
-		} catch (FileNotFoundException exception) { return false; }
-		catch (IOException exception) { return false; }
+		} catch (FileNotFoundException exception) {
+		  return false;
+		} catch (IOException exception) {
+		  return false;
+		}
 		
 		return true;
 	}
 	
-	private Object[] parse(String[] vals)
-	{
-		if (vals.length != types.length)
+	private Object[] parse(String[] vals) {
+		if (vals.length != types.length) {
 			return null;
+		}
 		Object[] result = new Object[vals.length];
-		for (int i = 0; i < vals.length; i++)
-		{
-			switch (types[i])
-			{
+		for (int i = 0; i < vals.length; i++) {
+			switch (types[i]) {
 				case INTEGER:
 					result[i] = new Integer(vals[i]);
 					break;
@@ -170,20 +158,21 @@ public class HighScoreEngine
 				case CHAR:
 					result[i] = new Character(vals[i].charAt(0));
 					break;
+				default:
+				  break;
 			}
 		}
 		return result;
 	}
 	
-	private boolean compare(Object val1, Object val2)
-	{
-		if (val1 instanceof String && val2 instanceof String)
+	private boolean compare(Object val1, Object val2) {
+		if (val1 instanceof String && val2 instanceof String) {
 			return ((String) val1).compareToIgnoreCase((String) val2) <= 0;
-		else if (val1 instanceof Integer && val2 instanceof Integer)
+		} else if (val1 instanceof Integer && val2 instanceof Integer) {
 			return ((Integer) val1) > ((Integer) val2);
-		else if (val1 instanceof Character && val2 instanceof Character)
+		} else if (val1 instanceof Character && val2 instanceof Character) {
 			return ((Character) val1) > ((Character) val2);
-		else
-			return false;
+		}
+		return false;
 	}
 }
--- src/com/jeffsul/minesweeper/highscores/HighScoreFilter.java
+++ src/com/jeffsul/minesweeper/highscores/HighScoreFilter.java
@@ -1,24 +1,20 @@
 package com.jeffsul.minesweeper.highscores;
 
-public class HighScoreFilter 
-{
+public class HighScoreFilter {
 	public static enum Filter {EQUAL, NOT_EQUAL};
 	
 	private int field;
 	private Object value;
 	private Filter criterion;
 	
-	public HighScoreFilter(int field, Object val, Filter crit)
-	{
+	public HighScoreFilter(int field, Object val, Filter crit) {
 		this.field = field;
 		value = val;
 		criterion = crit;
 	}
 	
-	public boolean match(Object[] vals)
-	{
-		switch (criterion)
-		{
+	public boolean match(Object[] vals) {
+		switch (criterion) {
 		case EQUAL:
 			return isEqual(vals[field], value);
 		case NOT_EQUAL:
@@ -28,13 +24,12 @@ public class HighScoreFilter
 		}
 	}
 	
-	private boolean isEqual(Object val1, Object val2)
-	{
-		if (val1 instanceof String && val2 instanceof String)
+	private boolean isEqual(Object val1, Object val2) {
+		if (val1 instanceof String && val2 instanceof String) {
 			return ((String) val1).equals((String) val2);
-		else if (val1 instanceof Integer && val2 instanceof Integer)
+		} else if (val1 instanceof Integer && val2 instanceof Integer) {
 			return ((Integer) val1).equals((Integer) val2);
-		else
-			return val1.equals(val2);
+		}
+		return val1.equals(val2);
 	}
 }
